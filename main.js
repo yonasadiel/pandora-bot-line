6d9b2a0ce301939d359a3a29ed68f7f272af8f80
yonasadiel <yonazadielwiguna@gmail.com>
2017-06-18 12:19:51 +0700

    use express

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(8080);
+var express = require('express')
+var app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)

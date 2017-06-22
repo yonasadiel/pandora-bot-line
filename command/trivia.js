4222393c4ef0baab94c04a07d9568623549b52b6
yonasadiel <yonazadielwiguna@gmail.com>
2017-06-22 16:58:34 +0700

    change mysql to fs

--- command/trivia.js
+++ command/trivia.js
@@ -17,10 +17,6 @@ function Session(id, question, correct_answer, incorrect_answer) {
     this.incorrect_answer = ["hai","pakabar?"];
   };
 
-	this.getId = function() {
-		return this.id;
-	};
-
 	this.getQuestion = function() {
 		var q   = "";
 		var opt = this.incorrect_answer.concat([this.correct_answer]);
@@ -39,14 +35,15 @@ function Session(id, question, correct_answer, incorrect_answer) {
 
 	this.getAnswer = function() {
 		return this.correct_answer;
-	}
+	};
 }
 
 module.exports = {
-	event      : "",
-	client     : "",
-	session    : "",
-	session_id : "",
+	event      : '',
+	client     : '',
+	session    : '',
+	session_id : '',
+  base_path  : '../data/trivia/',
 
 	receive  : function(argc, args, client, event) {
 		this.event  = event;
@@ -76,6 +73,8 @@ module.exports = {
 					return this.getNewQuestion();
 				case "answer":
 					return this.getSessionAnswer();
+        case "question":
+          return this.getLastQuestion();
 				default:
 					return client.replyMessage(event.replyToken, {
 						type : "text",
@@ -86,103 +85,49 @@ module.exports = {
 	},
 
 	getThisSession : function() {
-		var mysql = require('mysql');
+    var fs    = require('fs');
+    var path  = base_path + this.session_id;
 
-		var con = mysql.createConnection({
-		  host    : "localhost",
-		  user    : "root",
-		  password: "",
-      database: "mydb",
-		});
-
-		con.connect(function(err) {
-		  if (err) throw err;
-
-		  con.query("SELECT * FROM trivia WHERE id = " + this.session_id, function (err, result) {
-		    if (err) throw err;
-
-        if (result.length < 1) {
-          return this.makeNewSession();
-        } else {
-          return new Session(
-            result[0].id,
-            result[0].question,
-            result[0].correct_answer,
-            JSON.parse(result[0].incorrect_answer)
-          );
-        }
-		  });
-		});
+    if (!fs.existsSync(path)) {
+      return this.makeNewSession();
+    } else {
+      var result = JSON.parse(fs.readFileSync(path));
+      return new Session(
+        result.id,
+        result.question,
+        result.correct_answer,
+        result.incorrect_answer
+      );
+    }
 	},
 
   makeNewSession : function() {
-    var mysql = require('mysql');
+    var fs    = require('fs');
+    var path  = base_path + this.session_id;
 
-    var con = mysql.createConnection({
-      host    : "localhost",
-      user    : "root",
-      password: "",
-      database: "mydb",
-    });
+    var new_session = new Session(this.session_id, "", "", []);
+    fs.writeFileSync(path, JSON.stringify(new_session));
 
-    con.connect(function(err) {
-      if (err) throw err;
-
-      var query = "";
-
-      query += "INSERT INTO trivia";
-      query += " (id, question, correct_answer, incorrect_answer)";
-      query += " values";
-      query += " ('" + this.session_id + "', '', '', '[\"\"]')";
-
-      con.query(query, function (err, result) {
-        if (err) throw err;
-
-        return new Session(
-          this.session_id,
-          "",
-          "",
-          [],
-        );
-      });
-      
-    });
+    return new_session;
   },
 
 	getNewQuestion : function() {
 		this.session.update();
 
-    var mysql = require('mysql');
+    var fs    = require('fs');
+    var path  = base_path + this.session_id;
 
-    var con = mysql.createConnection({
-      host    : "localhost",
-      user    : "root",
-      password: "",
-      database: "mydb",
-    });
+    fs.writeFileSync(path, JSON.stringify(this.session));
 
-    con.connect(function(err) {
-      if (err) throw err;
-
-      var query = "";
-
-      query += "UPDATE trivia SET";
-      query += " question = '" + this.session.question + "'";
-      query += ", correct_answer='" + this.session.correct_answer + "'";
-      query += ", incorrect_answer='" + JSON.stringify(this.session.incorrect_answer) + "'";
-      query += " WHERE";
-      query += " id = " + this.session_id;
-
-      con.query(query, function (err, result) {
-        if (err) throw err;
-      });
-    });
-
-		return this.client.replyMessage(this.event.replyToken,{
-			type : "text",
-			text : this.session.getQuestion(),
-		});
+		return this.getLastQuestion();
 	},
+
+  getLastQuestion : function() {
+    return this.client.replyMessage(this.event.replyToken,{
+      type : "text",
+      text : this.session.getQuestion(),
+    }); 
+  },
 
 	getSessionAnswer : function() {
 
@@ -190,7 +135,7 @@ module.exports = {
 
 			return this.client.replyMessage(this.event.replyToken,{
 				type : "text",
-				text : this.sessions[session_index].getAnswer(),
+				text : this.session.getAnswer(),
 			});	
 
 		} else {

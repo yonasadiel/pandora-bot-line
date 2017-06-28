a4721f398873f318d7a5d2b4ddc5607cc097fa6e
yonasadiel <yonazadielwiguna@gmail.com>
2017-06-28 18:33:47 +0700

    update readme & help

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ See LINE official sdk [documentation](https://line.github.io/line-bot-sdk-nodejs
 
 ## How To Use
 
-Make the config.js according to config.js.example
+Make the .env according to .env.example
 
 * fill channel access token and channel secret according to line developer page
 * choose command symbol. only message starts with command symbol that will be processed
@@ -27,5 +27,3 @@ Make the config.js according to config.js.example
 Register every command at command folder.
 
 The filename should be based on the command that will be registered, without command symbol.
-
-See command/help.js for guide
--- command/about.js
+++ command/about.js
@@ -4,39 +4,6 @@
  * ------------------
  * Give an about reply about the bot
  * 
- * Every command file should have receive()
- * function with parameters:
- *   argc   : number of arguments
- *   args   : arguments presents on text message
- *   client : client object defined by line-bot-sdk
- *            for replying / etc
- *   event  : event webhook project that is sent
- *            by LINE messaging app, ex:
- *            "events": [
- *             {
- *               "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
- *               "type": "message",
- *               "timestamp": 1462629479859,
- *               "source": {
- *                 "type": "user",
- *                 "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
- *               },
- *               "message": {
- *                 "id": "325708",
- *                 "type": "text",
- *                 "text": "Hello, world"
- *               }
- *           },
- *             {
- *               "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
- *               "type": "follow",
- *               "timestamp": 1462629479859,
- *               "source": {
- *                 "type": "user",
- *                 "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
- *               }
- *             }
- *           ]
  */
 
 module.exports = {
@@ -50,7 +17,8 @@ module.exports = {
 		if (argc > 1) {
 			return this.sendResponse("don't understand the argument");
 		} else {
-      var txt = "Pandora Bot built by Yonas Adiel with NodeJS in HerokuApp\n";
+      let txt = 'Pandora Bot built by Yonas Adiel with NodeJS in HerokuApp\n';
+      txt += 'See the project at https://github.com/yonasadiel/pandora-bot-line';
 			return this.sendResponse(txt);
 		}
 	},

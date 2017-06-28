e8576f76896448d6c0fb1deba9a7440d1dc2f7b5
yonasadiel <yonazadielwiguna@gmail.com>
2017-06-28 18:00:11 +0700

    use env

---
+++ config.js
@@ -0,0 +1,24 @@
+
+/**
+ * Configuration File
+ * ------------------
+ * All variable that are needed
+ * for configuration will be
+ * presented here
+ */
+
+// you can get channel access token and channel secret in line developer page
+var channelAccessToken = proccess.env.CHANNEL_ACCESS_TOKEN;
+var channelSecret      = proccess.env.CHANNEL_SECRET;
+// Customize your own command symbol
+// only message prefixed with command symbol
+// that will be processed
+// You can leave it blank for processing all
+// message received
+var commandSymbol      = proccess.env.COMMAND_SYMBOL;
+
+module.exports = {
+	channelAccessToken : channelAccessToken,
+	channelSecret      : channelSecret,
+	commandSymbol      : commandSymbol
+};
